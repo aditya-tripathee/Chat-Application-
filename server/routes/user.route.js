9580e28054c16fa71c108a0f13759951ac7b190f
aditya-tripathee <adityatripatheee@gmail.com>
2026-04-01 17:58:05 +0530

    implemented routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { login, logout, register } from "../controllers/userController.js";
+import { getOtherUsers, login, logout, register } from "../controllers/userController.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register",register);
 userRouter.post("/login",login);
 userRouter.get("/logout",logout);
+userRouter.get("/",isAuthenticated,getOtherUsers);
 
 
 export default userRouter;
